d595adfc2933d23325cfb909422d8cc897eda72f
Azhar Khan <admin@azhark.com>
2022-04-27 15:14:06 +0530

    add example image

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 require_once "ImageDownload.php";
 
 $image = new ImageDownloader("images/");
-$downloadedimage = $image->getImage("");
+$downloadedimage = $image->getImage("https://avatars.githubusercontent.com/u/20738252");
 
 echo "Here is the downloaded image: <br>";
 echo "<img src='" . $downloadedimage . "'>";
